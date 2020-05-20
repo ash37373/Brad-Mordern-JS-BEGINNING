4e76cc9a1d59a353a6e1d6c7fefc2d83977c3aca
ash37373 <ash37373@gmail.com>
2020-05-21 01:34:01 +0900

    0521
    키보드, 입력 이벤트

---
+++ SECTION03-Dom-Events/app-3-31.js
@@ -0,0 +1,40 @@
+const form = document.querySelector("form");
+const taskInput = document.getElementById("task");
+
+//Clear input
+taskInput.value = "";
+
+// form.addEventListener("submit", runEvent);
+
+//Keydown;
+// taskInput.addEventListener("keydown", runEvent);
+//keyup;
+// taskInput.addEventListener("keyup", runEvent);
+//keypress
+// taskInput.addEventListener("keypress", runEvent);
+//focus
+// taskInput.addEventListener("focus", runEvent);
+//Blur
+// taskInput.addEventListener("blur", runEvent);
+// Cut
+// taskInput.addEventListener('cut', runEvent);
+// Paste
+// taskInput.addEventListener('paste', runEvent);
+// Input
+// taskInput.addEventListener('input', runEvent);
+// Change
+// select.addEventListener("change", runEvent);
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  /* if (e.type === "keydown") {
+    document.body.style.background = "red";
+  }
+  if (e.type === "keyup") {
+    document.body.style.background = "blue";
+  } */
+
+  // console.log(taskInput.value);
+
+  e.preventDefault();
+}
